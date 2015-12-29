3529079ab24986c030bfb63f740dea57d304bf7c
Kyle Gosen <gosenkyl@msu.edu>
2015-12-29 01:28:13 -0500

    Header Changes

--- nfl.php
+++ nfl.php
@@ -5,4 +5,7 @@ include_once 'TeamHolder.php';
 $teamHolder = TeamHolder::getInstance();
 $teams =  $teamHolder->getNFLTeams();
 
+header('Access-Control-Allow-Origin: *');
+header('content-type: application/json; charset=utf-8');
+
 echo json_encode($teams);
