17f79a9214a26488eb3d679d8174b34dbf428225
Kyle Gosen <gosenkyl@msu.edu>
2015-12-29 01:36:25 -0500

    MLB Headers

--- mlb.php
+++ mlb.php
@@ -5,4 +5,8 @@ include_once 'TeamHolder.php';
 $teamHolder = TeamHolder::getInstance();
 $teams =  $teamHolder->getMLBTeams();
 
+
+header('Access-Control-Allow-Origin: *');
+header('content-type: application/json; charset=utf-8');
+
 echo json_encode($teams);
